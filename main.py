9403ffa71e3e463b4a3ff0bc678777fe968f59c3
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2023-03-18 13:55:41 -0500

    prompt tried and changed the bot layout

--- main.py
+++ main.py
@@ -2,43 +2,62 @@ import sys
 import openai
 import os
 from dotenv import load_dotenv
+from colorama import Fore, Back, Style
 
-load_dotenv() # load environment variables from .env file
+load_dotenv()  # load environment variables from .env file
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def generate_response(prompt):
+# define prompts
+prompt = [
+    "Tell me about yourself.",
+    "What are your hobbies?",
+    "Why do you want to work for our company?",
+    "What are your strengths?",
+    "What are your weaknesses?",
+    "What are your salary expectations?",
+    "What are your career goals?",
+    "What are your long-term goals?",
+    "What are your short-term goals?",
+    "Do you have any questions for me?",
+    "Why should we hire you?",
+    "What are your greatest achievements?",
+]
+
+
+def generate_response(prompt, chat_history=None):
     response = openai.Completion.create(
-        engine = "text-davinci-003",
-        prompt = prompt,
-        temperature = 0.6,                      # Temperature is the randomness of the model.
-                                                # The higher the temperature, the more random the text.
-                                                # The lower the temperature, the less random the text.
-                                                # 0.9 is the default value.
-        max_tokens = 1024,
-        top_p = 1,
-        frequency_penalty = 0,
-        presence_penalty = 0,
+        engine="text-davinci-003",
+        prompt=prompt,
+        # Temperature is the randomness of the model.
+        temperature=0.7,
+        # The higher the temperature, the more random the text.
+        # The lower the temperature, the less random the text.
+        # 0.9 is the default value.
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
     )
-    return response.choices[0].text.strip()
+    message = response.choices[0].text.strip()
+    if chat_history is not None:
+        chat_history.append((prompt, message))
+    return message
 
-while True:
-    # Get user input
-    user_input = input("You: ")
+def start_chat():
+    print("Welcome to the Job Interview Chatbot!")
+    while True:
+        chat_history = []
+        # Get user input
+        user_input = input(Fore.GREEN + Style.BRIGHT + "You: " + Style.RESET_ALL).strip()
+        # Exit if user types "exit"
+        if user_input.lower() in ["exit", "quit", "bye", "goodbye"]:
+            print(Fore.RED + Style.BRIGHT + "Bot: Bye!")
+            break
+        # prompt = f"User: {user_input}\nBot: "
+        prompt = user_input
+        # Generate response
+        response = generate_response(prompt, chat_history)
+        print(Fore.CYAN + Style.BRIGHT + "Bot: " + Style.NORMAL + response)
 
-    # Exit if user types "exit"
-    if user_input.lower() == "exit":
-        print("Bot: Bye!")
-        sys.exit()
-    elif user_input.lower() == "close":
-        print("Bot: Closing...")
-        sys.exit()
-    elif user_input.lower() == "clear":
-        os.system("cls")
-        continue
-
-    prompt = f"User: {user_input}\nBot: "
-    # Generate response
-    response = generate_response(prompt)
-    # Print response
-    print("Bot: ",response)
+start_chat()
